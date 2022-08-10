624142b51a9641a10c84558f7331fe3e236c0fd0
jA71 <isjavier77@gmail.com>
2022-08-10 15:48:58 -0500

    Se sube codigo para busqueda

--- app/src/main/java/com/example/login_firebase/CodeOptions.kt
+++ app/src/main/java/com/example/login_firebase/CodeOptions.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.login_firebase.databinding.ActivityCodeOptionsBinding
+import com.example.login_firebase.dto.Code
+import com.example.login_firebase.dto.Send
+import com.example.login_firebase.dto.WsClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import androidx.appcompat.widget.SearchView
 
 class CodeOptions : AppCompatActivity() {
 
     private lateinit var views: ActivityCodeOptionsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        views = ActivityCodeOptionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        views = ActivityCodeOptionsBinding.inflate(layoutInflater)
         setContentView(views.root)
         setSupportActionBar(views.toolbar)
         supportFragmentManager.beginTransaction().replace(views.fragmento.id, listadoRedesCode())
@@ -20,55 +29,6 @@ class CodeOptions : AppCompatActivity() {
         accionesMenuBajo()
         accionesMenuArriba()
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.principal, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-//    private fun enableFinder(item: MenuItem) {
-//        val finder = item.actionView as androidx.appcompat.widget.SearchView
-//        finder.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                searching(query.toString().trim())
-//                return false
-//            }
-//
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                searching(newText.toString().trim())
-//                return false
-//            }
-//        })
-//    }
-
-//    private fun searching(text: String) {
-//        text?.let { realText ->
-//            if (realText.length >= 3) {
-//                WsClient.apiSocial()?.findMail(Send.Builder().name(realText).build())
-//                    ?.enqueue(object : Callback<List<Mail>> {
-//                        override fun onResponse(
-//                            call: Call<List<Mail>>,
-//                            response: Response<List<Mail>>
-//                        ) {
-//                            if (response.isSuccessful) {
-//                                response.body()?.let { list -> fillFragments(list) }
-//                            } else {
-//                                views.listadoOpciones.adapter = null
-//                            }
-//                        }
-//
-//                        override fun onFailure(call: Call<List<Mail>>, t: Throwable) {
-//                            Toast.makeText(this@MailOptions, "Error, No data found", Toast.LENGTH_SHORT).show()
-//                        }
-//
-//                    })
-//            } else {
-//                views.fragmento.adapter = null
-//            }
-//
-//        }
-//    }
-
 
     private fun accionesMenuArriba() {
         views.toolbar.setOnMenuItemClickListener { itemArriba ->
@@ -109,6 +69,63 @@ class CodeOptions : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    // BUSCADOR
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.principal, menu)
+        enableFinder(menu.findItem(R.id.buscar))
+        return true
+    }
+
+    private fun enableFinder(item: MenuItem) {
+        val finder = item.actionView as SearchView
+        finder.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searching(query.toString().trim())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searching(newText.toString().trim())
+                return false
+            }
+        })
+    }
+
+    private fun searching(text: String) {
+        text?.let { realText ->
+            if (realText.length >= 3) {
+                WsClient.apiSocial()?.findCode(Send.Builder().name(realText).build())
+                    ?.enqueue(object : Callback<List<Code>> {
+                        override fun onResponse(
+                            call: Call<List<Code>>,
+                            response: Response<List<Code>>
+                        ) {
+                            if (response.isSuccessful) {
+                                response.body()?.let { list ->
+                                    fillFragments(list)
+                                }
+                            } else {
+                                views.fragmento.adapter = null
+                            }
+                        }
+
+                        override fun onFailure(call: Call<List<Code>>, t: Throwable) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+            } else {
+                views.fragmento.adapter = null
+            }
+        }
+    }
+
+    private fun fillFragments(list: List<Code>) {
+        views.fragmento.layoutManager = LinearLayoutManager(this)
+        views.fragmento.adapter = AdapterCode(list)
     }
 
 
--- app/src/main/java/com/example/login_firebase/listadoRedesCode.kt
+++ app/src/main/java/com/example/login_firebase/listadoRedesCode.kt
@@ -2,14 +2,14 @@ package com.example.login_firebase
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.login_firebase.databinding.FragmentListadoRedesCodeBinding
 import com.example.login_firebase.dto.Code
+import com.example.login_firebase.dto.Send
 import com.example.login_firebase.dto.WsClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -48,4 +48,8 @@ class listadoRedesCode : Fragment() {
             }
         })
     }
+
+
+
+
 }
